1419668b3a5cee54d151fbd8010b11ccd27ef958
lancy1014@gmail.com <lancy1014@gmail.com>
2012-02-19 16:50:46 +0800

    Initial Commit

---
+++ party/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  party
+//
+//  Created by Lancy on 12-2-19.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
